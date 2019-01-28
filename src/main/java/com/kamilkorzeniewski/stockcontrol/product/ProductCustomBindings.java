826241d366853456273cb9599b5d5fecc78196cb
kamilkorzeniewski <kamilkorzeniewski13@gmail.com>
2019-01-28 23:46:42 +0100

    add pom file

--- src/main/java/com/kamilkorzeniewski/stockcontrol/product/ProductCustomBindings.java
+++ src/main/java/com/kamilkorzeniewski/stockcontrol/product/ProductCustomBindings.java
@@ -1,6 +1,8 @@
 package com.kamilkorzeniewski.stockcontrol.product;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
@@ -12,9 +14,7 @@ public class ProductCustomBindings {
     public static void bind(QuerydslBindings bindings) {
         bindings.bind(String.class).all((StringPath path, Collection<? extends String> values) -> {
             BooleanBuilder predicate = new BooleanBuilder();
-            values.forEach(value -> {
-                if (value.startsWith("*") && value.endsWith("*")) {
-                    predicate.or(path.containsIgnoreCase(value.substring(1,value.length() - 1)));
+            values.forEach(value -> { if (value.startsWith("*") && value.endsWith("*")) { predicate.or(path.containsIgnoreCase(value.substring(1,value.length() - 1)));
                 }
                 if(value.startsWith("*")){
                     predicate.or(path.endsWithIgnoreCase(value.substring(1, value.length())));

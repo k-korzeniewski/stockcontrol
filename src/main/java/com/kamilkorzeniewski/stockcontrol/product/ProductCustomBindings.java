1df351c5c1167bf950f46cd229b57b665961d44e
kamilkorzeniewski <kamilkorzeniewski13@gmail.com>
2019-01-28 23:48:54 +0100

    Change pom file (remove vaadin dependency)

--- src/main/java/com/kamilkorzeniewski/stockcontrol/product/ProductCustomBindings.java
+++ src/main/java/com/kamilkorzeniewski/stockcontrol/product/ProductCustomBindings.java
@@ -1,8 +1,6 @@
 package com.kamilkorzeniewski.stockcontrol.product;
 
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Expression;
-import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
@@ -14,12 +12,14 @@ public class ProductCustomBindings {
     public static void bind(QuerydslBindings bindings) {
         bindings.bind(String.class).all((StringPath path, Collection<? extends String> values) -> {
             BooleanBuilder predicate = new BooleanBuilder();
-            values.forEach(value -> { if (value.startsWith("*") && value.endsWith("*")) { predicate.or(path.containsIgnoreCase(value.substring(1,value.length() - 1)));
+            values.forEach(value -> {
+                if (value.startsWith("*") && value.endsWith("*")) {
+                    predicate.or(path.containsIgnoreCase(value.substring(1, value.length() - 1)));
                 }
-                if(value.startsWith("*")){
+                if (value.startsWith("*")) {
                     predicate.or(path.endsWithIgnoreCase(value.substring(1, value.length())));
                 }
-                if(value.endsWith("*")){
+                if (value.endsWith("*")) {
                     predicate.or(path.startsWithIgnoreCase(value.substring(0, value.length() - 1)));
                 }
             });
